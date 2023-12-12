8ec7c419a9127a351300fa8da24f551892528bb6
Nwamini Emmanuel Onyedikachi <emmanuelgodwin558@gmail.com>
2023-12-12 03:57:59 +0100

    added

--- control_flow.py
+++ control_flow.py
@@ -1,15 +1,58 @@
-bill_total = 95 
-discount = 10
+bill_total = 210
 
-if bill_total > 100:
-    print('You get a discount of 10%')
-    bill_total = bill_total - (bill_total * discount / 100)
-    print(f'Your total bill is $ {bill_total:.2f}')
+discount1 = 10
+discount2 = 20
+
+# if bill_total > 100:
+#     print('You get a discount of 10%')
+#     bill_total = bill_total - (bill_total * discount / 100)
+#     print(f'Your total bill is $ {bill_total:.2f}')
     
-if bill_total > 100:
+if bill_total > 100 and bill_total < 200:
         print("Bill is grater than 100")
-        bill_total = bill_total - discount
+        bill_total = bill_total - discount1
+elif bill_total > 200:
+            print("Bill is greater than 200")
+            bill_total = bill_total - discount2
 else:
         print("Bill is less than 100")
         
-        print("Total bill:" + str(bill_total))
+print("Total bill:" + str(bill_total))
+
+
+
+#Light is currently off
+current = False
+
+if current:
+    current = False
+    print('Turning light off')
+
+if not current:
+    current = True
+    print('Turning light on')
+
+current = False
+
+if current:
+    current = False
+    print('Turning light off')
+else: 
+    current = True
+    print('Turning light on')
+
+
+loyalty_customer = True
+total_bill = 124
+
+if loyalty_customer and total_bill > 100:
+    #give 20% discount
+    total_bill = total_bill - (float(total_bill)/ 100) * 20
+elif total_bill > 100:
+    #give 10% discount
+    total_bill = total_bill - (float(total_bill)/ 100) * 10
+else:
+    #sorry no discount, 5% service charge applied.
+    print('Sorry, no discount ...')
+
+print('Total Bill: ', float(total_bill))
