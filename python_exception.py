40e8acac2b3021718224294f496ecd03c2072745
Nwamini Emmanuel Onyedikachi <emmanuelgodwin558@gmail.com>
2023-12-14 04:25:09 +0100

    updated

--- python_exception.py
+++ python_exception.py
@@ -14,3 +14,29 @@ print(ans)
 # Starter code
 with open('file_does_not_exist.txt', 'r') as file:
     print(file.read())
+
+
+try:
+    item = items[6]
+    print(item)
+except: 
+    print("The index does not exist in the list.")
+
+
+def divide_by(a, b):
+    try:
+        return a / b
+    except ZeroDivisionError:
+        return 0
+    except Exception as e:
+        print(e, 'Something went wrong!')
+
+ans = divide_by(10,0)
+print(ans)
+
+
+try:
+    with open('file_does_not_exist.txt', 'r') as file:
+        print(file.read())
+except:
+    print("Unable to locate file")  
