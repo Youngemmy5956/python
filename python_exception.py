b9f6eebc22f407c33a3fa14dbe472ab6f0e90dac
Nwamini Emmanuel Onyedikachi <emmanuelgodwin558@gmail.com>
2023-12-14 04:23:56 +0100

    added

---
+++ python_exception.py
@@ -0,0 +1,16 @@
+# Starter code
+items = [1,2,3,4,5]
+item = items[6]
+print(item)
+
+# Starter code
+def divide_by(a, b):
+    return a / b
+
+
+ans = divide_by(40, 0)
+print(ans)
+
+# Starter code
+with open('file_does_not_exist.txt', 'r') as file:
+    print(file.read())
