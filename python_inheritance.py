06f6806186d30e9fd59833f8822cf997dfd972aa
Nwamini Emmanuel Onyedikachi <emmanuelgodwin558@gmail.com>
2023-12-16 06:15:27 +0100

    updated

---
+++ python_inheritance.py
@@ -0,0 +1,60 @@
+Class A:
+    pass
+Class B(A):
+    pass
+
+
+
+
+# Example 1
+class A:
+   a = 1
+   
+class B:
+   b = 2
+   
+class C(A, B):
+   pass
+
+c = C()
+print(c.a, c.b)
+
+
+class A:
+   a = 1
+
+class B(A):
+   a = 2
+
+class C(B):
+   pass
+
+c = C()
+print(c.a)
+
+issubclass(class A, class B)
+
+print(issubclass(A,B))
+print(issubclass(B,A))
+
+Class A:
+	pass
+Class B(A):
+	pass
+
+b = B()
+print(isinstance(b,B))
+print(isinstance(b,B))
+
+
+class Fruit():
+    def __init__(self, fruit):
+        print('Fruit type: ', fruit)
+
+
+class FruitFlavour(Fruit):
+    def __init__(self):
+        super().__init__('Apple')
+        print('Apple is sweet')
+
+apple = FruitFlavour()
